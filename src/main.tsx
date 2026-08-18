c1f003fb56332984014af7f175474e7811fe23fe
almd-blip <isildalmd@gmail.com>
2026-08-18 01:15:53 +0100

    Apply consistent left alignment to drafting context

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import './quiet-drafting-desk.css';
 import './restore-writing-alignment.css';
 import './quiet-drafting-desk-final.css';
+import './align-desk-context.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
