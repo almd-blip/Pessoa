90741538df1dfd8f956291cd221c4f3f8c3c6eb1
almd-blip <isildalmd@gmail.com>
2026-08-18 01:14:21 +0100

    Apply final Project selector alignment

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './quiet-drafting-desk.css';
 import './restore-writing-alignment.css';
+import './quiet-drafting-desk-final.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
