3c84a22917e0a908d1d7e9440782ecc979debbe4
almd-blip <isildalmd@gmail.com>
2026-08-18 01:11:23 +0100

    Apply writing alignment restoration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './quiet-drafting-desk.css';
+import './restore-writing-alignment.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
