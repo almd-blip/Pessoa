f5bb717c1d80da262065d52c7d0252e0a797f756
almd-blip <isildalmd@gmail.com>
2026-08-18 01:23:37 +0100

    Apply exact Quiet Drafting Desk layout

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './quiet-drafting-desk.css';
 import './restore-writing-alignment.css';
 import './quiet-drafting-desk-final.css';
 import './align-desk-context.css';
+import './quiet-drafting-desk-layout.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
